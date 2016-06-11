ee9b0b22213e34624fbafabbd6a1d8ef3f7fce41
Tomasz Krzyszkowski <tomek.krzyszko@gmail.com>
2016-06-11 13:53:10 +0200

    Add first api method

--- app/src/main/java/pl/marczak/tauronrealityhack/networking/ApiService.java
+++ app/src/main/java/pl/marczak/tauronrealityhack/networking/ApiService.java
@@ -1,15 +1,13 @@
 package pl.marczak.tauronrealityhack.networking;
 
+import java.util.List;
+
 import pl.marczak.tauronrealityhack.model.SectorResponse;
 import retrofit.Callback;
 import retrofit.http.GET;
 
-/**
- * Created by Admin on 2016-06-11.
- */
 public interface ApiService {
 
-    @GET(":8084/SystemKurierski/sectors")
-    void getSensor( Callback<SectorResponse> callback);
-    ///SystemKurierski/sectors
+    @GET("/SystemKurierski/sectors")
+    void getSensor( Callback<List<SectorResponse>> callback);
 }
