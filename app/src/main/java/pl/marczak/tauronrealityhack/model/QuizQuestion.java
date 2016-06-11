faac06d75f77b0caac1e34fb84a261f6e06e75d2
Johannes Planis <johannes.planis@gmail.com>
2016-06-11 15:55:57 +0200

    quizy działają

--- app/src/main/java/pl/marczak/tauronrealityhack/model/QuizQuestion.java
+++ app/src/main/java/pl/marczak/tauronrealityhack/model/QuizQuestion.java
@@ -1,14 +1,16 @@
 package pl.marczak.tauronrealityhack.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Admin on 2016-06-11.
  */
-public class QuizQuestion {
+public class QuizQuestion implements Serializable{
     private int id;
     private String question;
+    private List<QuizAnswer> answers = new ArrayList<>();
 
     public List<QuizAnswer> getAnswers() {
         return answers;
@@ -16,6 +18,15 @@ public class QuizQuestion {
 
     public void setAnswers(List<QuizAnswer> answers) {
         this.answers = answers;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getQuestion() {
@@ -26,29 +37,10 @@ public class QuizQuestion {
         this.question = question;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public QuizQuestion(int id, String question, List<QuizAnswer> answers) {
+
         this.id = id;
         this.question = question;
         this.answers = answers;
     }
-
-    @Override
-    public String toString() {
-        return "QuizQuestion{" +
-                "id=" + id +
-                ", question='" + question + '\'' +
-                ", answers=" + answers +
-                '}';
-    }
-
-    private List<QuizAnswer> answers = new ArrayList<>();
-
 }
